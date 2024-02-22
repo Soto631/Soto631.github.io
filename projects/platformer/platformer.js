69aea1ff7cf458d9eccd2cf2fd50931931271e13
Soto631 <157506255+Soto631@users.noreply.github.com>
2024-02-22 15:43:58 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -46,27 +46,27 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-    createPlatform(0, 180, 175, 15);
-    createPlatform(355, 180, 200, 15)
-    createPlatform(700, 180, 200, 15)
-    createPlatform(875, 180, 200, 15)
+    createPlatform(0, 200, 175, 15);
+    createPlatform(355, 200, 200, 15)
+    createPlatform(700, 200, 200, 15)
+    createPlatform(875, 200, 200, 15)
     createPlatform(1300, 250, 100, 15)
     createPlatform(1050, 450, 900, 15)
-    createPlatform(375, 375, 500, 15)
-    createPlatform(0, 320, 165, 15)
-    createPlatform(225, 495, 200, 15)
-    createPlatform()
+    createPlatform(375, 400, 500, 15)
+    createPlatform(0, 345, 165, 15)
+    createPlatform(395, 645, 200, 15)
+    createPlatform(695, 685, 200, 15)
+    createPlatform(0,600, 200, 15)
    
-
-
-
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+    createCollectable("steve", 1300, 200,);
+    createCollectable("grace", 0, 300,);
+    createCollectable("max", 0, 550,);
 
 
 
